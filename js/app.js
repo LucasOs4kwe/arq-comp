ab4892f2cb23aff5b730a3b725b944b6cd2c4c2a
LucasOs4kwe <LUCAS.SSANTOS@bandtec.com.br>
2021-11-02 22:35:26 -0300

    Lógica em JS pronta

--- js/app.js
+++ js/app.js
@@ -1,9 +1,36 @@
 
-// INDEX
-document.getElementById('first-btn').addEventListener("click", function(){
-    window.location.href = 'second.html';
-})
+// // INDEX
+// document.getElementById('first-btn').addEventListener("click", function(){
+//     window.location.href = 'second.html';
+// })
 
 
 
 // SECOND
+
+function v1(){
+    var word = ["R","A","M"];
+    var l1 = document.getElementById('inpt1').value;
+    var l2 = document.getElementById('inpt2').value;
+    var l3 = document.getElementById('inpt3').value;
+    var palavra = [l1,l2,l3];
+    for(var c = 0; c < word.length; c++){
+        if(palavra[c] == word[c]){
+            document.getElementById('inpt1').style.backgroundColor = 'green';
+            document.getElementById('inpt2').style.backgroundColor = 'green';
+            document.getElementById('inpt3').style.backgroundColor = 'green';
+            var dis = document.getElementById('inpt1');
+            var dis2 = document.getElementById('inpt2');
+            var dis3 = document.getElementById('inpt3');
+            dis.disabled = true;
+            dis2.disabled = true;
+            dis3.disabled = true;
+        }else{
+            alert('Resposta errada!');
+            inpt1.value = '';
+            inpt2.value = '';
+            inpt3.value = '';
+            break;
+        }
+    }
+}
