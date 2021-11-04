41700dabcdad2d97762d8f1227dbe6d238d7504b
LucasOs4kwe <LUCAS.SSANTOS@bandtec.com.br>
2021-11-04 20:35:59 -0300

    Lógica em JS melhorada. Implementação de laços for

--- js/app.js
+++ js/app.js
@@ -8,33 +8,30 @@ document.getElementById('first-btn').addEventListener("click", function(){
 
 // SECOND
 
+
+// RAM 
 function v1(){
     var word = ["R","A","M"];
-    var l1 = document.getElementById('inpt1').value;
-    var l2 = document.getElementById('inpt2').value;
-    var l3 = document.getElementById('inpt3').value;
-    var palavra = [l1,l2,l3];
+    var palavra = [];
     var i = 0;
+    var x = document.getElementsByClassName("inpt_ram");
+    for(var l = 0; l < x.length; l++) {
+        palavra += [x[l].value];
+    }  
     for(var c = 0; c < word.length; c++){
         if(palavra[c] == word[c]){
             i++;
         }
     }
     if(i == 3){
-        document.getElementById('inpt1').style.backgroundColor = 'green';
-        document.getElementById('inpt2').style.backgroundColor = 'green';
-        document.getElementById('inpt3').style.backgroundColor = 'green';
-        var dis = document.getElementById('inpt1');
-        var dis2 = document.getElementById('inpt2');
-        var dis3 = document.getElementById('inpt3');
-        dis.disabled = true;
-        dis2.disabled = true;
-        dis3.disabled = true;
+        for(var o = 0; o < x.length; o++){
+            x[o].style.backgroundColor = 'green';
+            x[o].disabled = true;
+        }
     }else if(i != 3){
-        inpt1.value = '';
-        inpt2.value = '';
-        inpt3.value = '';
+        for(var p = 0; p < x.length; p++){
+            x[p].value = '';
+        }
         alert('Resposta errada!')
     }
-    // FAZER O BOTÃO SUMIR E APARECER UM ITEM CHECK NO LUGAR QND A RESPOSTA ESTIVER CORRETA
 }
